ed6298465ac7b6d1d3ed54974b4e943052bf7792
Dan Ellis <daniel.ellis.research@googlemail.com>
2017-07-19 00:03:45 +0100

    rosie oled

---
+++ OLED/adf_bitmap.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Image to Adafruit OLED Bitmap Converter</title>
+<meta http-equiv=content-type content="text/html; charset=UTF-8">
+</head>
+
+<body>
+	<h1>Image to Adafruit OLED Bitmap Converter</h1>
+	
+	<form enctype="multipart/form-data" action="adf_bitmap.php" method="POST">
+	<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+		Choose a file to convert: <input name="uploadedfile" type="file" /><br />
+	<input type="submit" value="Convert File" />
+</form>
+
+
+</body>
+</html>
